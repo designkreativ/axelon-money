46cf7173b36b452791fdbb63c68d791630939f2c
Musa Mwanza <dmmk.creations.21@gmail.com>
2025-03-17 14:12:49 +0300

    Product sections

---
+++ src/sections/Product1.tsx
@@ -0,0 +1,39 @@
+import localFont from "next/font/local";
+import ProductImage1 from "@/assets/product_image_1.png";
+import ProductImage2 from "@/assets/product_image_2.png";
+import Image from "next/image";
+
+const Supremell = localFont({
+  src: "../font/supremell/SupremeLL-Bold.otf",
+});
+
+export const Product1 = () => {
+  return (
+    <section>
+      <div className="container wrapper max-w-[1320px]  py-24 ">
+        <div className="max-w-[768px] mx-auto pb-10 md:pb-20 flex flex-col items-center">
+          <p className="text-center px px-3 py-0.5 rounded-xl font-normal gap-2 border  border-neutral-300">
+            Available for Download
+          </p>
+          <div className={`${Supremell.className}`}>
+            <h2 className="text-center text-gradient text-3xl md:text-5xl tracking-tight mt-2 pb-4 ">
+              One app for all your money things
+            </h2>
+          </div>
+          <p className="text-center text-xl text-[var(--primary-10)] mt-2 ">
+            From your everyday spending, to planning for your future with
+            savings and investments. Axelon helps you get more your money
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row  items-center justify-center gap-4">
+          <Image
+            className="not-md:w-full "
+            src={ProductImage1}
+            alt="Product Image 1"
+          />
+          <Image className="" src={ProductImage2} alt="Product Image 2" />
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/sections/Product2.tsx
@@ -0,0 +1,42 @@
+import localFont from "next/font/local";
+import ProductImage3 from "@/assets/product_image_3.png";
+import ProductImage4 from "@/assets/product_image_4.png";
+import ProductImage5 from "@/assets/product_image_5.png";
+import Image from "next/image";
+
+const Supremell = localFont({
+  src: "../font/supremell/SupremeLL-Bold.otf",
+});
+
+export const Product2 = () => {
+  return (
+    <section>
+      <div className="container wrapper max-w-[1320px] py-24 ">
+        <div className="flex flex-col lg:flex-row lg:items-center lg:gap-10 pb-10 ">
+          <div className=" flex flex-col items-start">
+
+          <p className="text-center px px-3 py-0.5 rounded-xl font-normal gap-2 border  border-neutral-300">
+            Try it now!
+          </p>
+
+          <div className={`${Supremell.className}`}>
+            <h2 className=" text-gradient text-3xl md:text-5xl tracking-tight mt-2 pb-4 lg:max-w-md">
+              Make your spend, Well spent
+            </h2>
+          </div>
+          </div>
+
+          <p className="flex-1 text-xl text-[var(--primary-10)] mt-2 lg:max-w-lg">
+            Manages a diversified group of specialized private credit brands
+            with efficient tech enabled processes.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row items-center justify-center gap-4">
+          <Image className="not-md:w-full" src={ProductImage3} alt="Product Image 3" />
+          <Image className="not-md:w-full" src={ProductImage4} alt="Product Image 3" />
+          <Image className="not-md:w-full" src={ProductImage5} alt="Product Image 3" />
+        </div>
+      </div>
+    </section>
+  );
+};
