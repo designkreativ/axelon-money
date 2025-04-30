b1776336d14411019cc24b6c283fd0998e2a5276
Musa Mwanza <dmmk.creations.21@gmail.com>
2025-04-30 21:14:51 +0300

    fix

--- src/components/FaqsText.tsx
+++ src/components/FaqsText.tsx
@@ -4,8 +4,13 @@ import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 
 import { useState } from "react";
+type FaqsTextProps = {
+  key: string;
+  question: string,
+  answer: string
+}
 
-export default function FaqsText({ key, question, answer }: any) {
+export default function FaqsText(props: FaqsTextProps) {
   const [isShown, notShown] = useState(false);
   function toggle() {
     notShown((prev) => !prev);
@@ -15,13 +20,13 @@ export default function FaqsText({ key, question, answer }: any) {
   return (
     <div className="pb-4">
       <div
-        key={key}
+        key={props.key}
         className={twMerge( "py-4 px-6 bg-[var(--primary-100)] rounded-2xl",  isShown && "bg-white  shadow-xl shadow-blue-900/5")}
         onClick={toggle}
       >
         <div className="flex flex-row justify-between items-start">
           <h3 className="text-xl font-normal leading-8 text-[var(--primary-10)] ">
-            {question}
+            {props.question}
           </h3>
           <Image
             src={PlusIcon}
@@ -31,7 +36,7 @@ export default function FaqsText({ key, question, answer }: any) {
             className={twMerge("pt-2 opacity-10 hover:opacity-100", isShown && "rotate-45")}
           />
         </div>
-        <p className={twMerge(isShown && "text-lg pt-4 text-[var(--shades-50)]")}>{isShown && answer}</p>
+        <p className={twMerge(isShown && "text-lg pt-4 text-[var(--shades-50)]")}>{isShown && props.answer}</p>
       </div>
     </div>
   );
